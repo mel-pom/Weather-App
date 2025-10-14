7300957ffc74811a1b754a97eed7a2ff2819f8c8
mel-pom <melpomerleau@gmail.com>
2025-10-14 13:49:53 -0700

    first commit

---
+++ src/components/Weather.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import './Weather.css'
+
+
+const Weather = () => {
+  return (
+    <div className='weather'>
+        <div className="search-bar">
+            <input type="text" placeholder='Search' />
+            
+        </div>
+    </div>
+  )
+}
+
+export default Weather
